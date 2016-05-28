9d5dec6c1ae03b05a9ded3703006f9fa7fee9dbe
teamalpha5441 <teamalpha5441@gmail.com>
2016-05-28 17:59:18 +0200

    CPU usage is now fetched for each core

--- src_linux/StatCollector.cs
+++ src_linux/StatCollector.cs
@@ -34,15 +34,16 @@ namespace srvmon
 
         private bool FirstUpdate;
         private string[] NetworkInterfaces;
-        private ulong CPU_TotalJiffies;
-        private ulong CPU_WorkJiffies;
+        private ulong[] CPU_TotalJiffies;
+        private ulong[] CPU_WorkJiffies;
         private ulong Mem_Total;
         private Dictionary<string, string> HardDrives;
+        private readonly byte CpuCount;
         private float RareUpdateElapsedSeconds;
 
         public Dictionary<string, NetworkInterfaceStats> NetworkStats { get; private set; }
         public DateTime BootTime { get; private set; }
-        public float[] CPU_Usage { get; private set; }
+        public List<float[]> CPU_Usage { get; private set; }
         public float[] RAM_Usage { get; private set; }
         public SysStatus SystemdStatus { get; private set; }
         public List<string> SystemdFailedUnits { get; private set; }
@@ -63,6 +64,7 @@ namespace srvmon
             if (this.Mem_Total < 1)
                 throw new IOException("Couldn't read total memory amount");
             this.HardDrives = HardDrives;
+            this.CpuCount = (byte)(Environment.ProcessorCount + 1);
             this.RareUpdateElapsedSeconds = 0;
 
             // prepare public fields
@@ -81,7 +83,9 @@ namespace srvmon
             }
             if (!bootTimeSet)
                 throw new IOException("Couldn't read boot time");
-            this.CPU_Usage = new float[ARRAY_BUFFER_LENGTH];
+            this.CPU_Usage = new List<float[]>(this.CpuCount);
+            for (byte i = 0; i < this.CpuCount; i++)
+                this.CPU_Usage.Add(new float[ARRAY_BUFFER_LENGTH]);
             this.RAM_Usage = new float[ARRAY_BUFFER_LENGTH];
             this.SystemdFailedUnits = new List<string>();
             this.HardDriveStatus = new Dictionary<string, HDDStatus>(HardDrives.Count);
@@ -125,28 +129,45 @@ namespace srvmon
                 }
             }
 
-            ulong CPU_TotalJiffies = 0, CPU_WorkJiffies = 0;
-            foreach (string line in File.ReadAllLines("/proc/stat"))
+            ulong[] CPU_TotalJiffies = new ulong[this.CpuCount];
+            ulong[] CPU_WorkJiffies = new ulong[this.CpuCount];
             {
-                int indexOfSpace = line.IndexOf(' ');
-                if (line.Substring(0, indexOfSpace) == "cpu")
+                string[] lines = File.ReadAllLines("/proc/stat");
+                for (byte i = 0; i < this.CpuCount; i++)
                 {
-                    string[] fields = line.Substring(indexOfSpace + 1).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    for (byte i = 0; i < fields.Length; i++)
-                    {
-                        ulong value = Convert.ToUInt64(fields[i]);
-                        CPU_TotalJiffies += value;
-                        if (i < 3)
-                            CPU_WorkJiffies += value;
-                    }
-                    break;
+                    int indexOfSpace = lines[i].IndexOf(' ');
+                    string[] fields = lines[i].Substring(indexOfSpace + 1).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // see https://github.com/hishamhm/htop/blob/e0209da88faf3b390d71ff174065abd407abfdfd/ProcessList.c#L942
+
+                    ulong usertime = Convert.ToUInt64(fields[0]);
+                    ulong nicetime = Convert.ToUInt64(fields[1]);
+                    ulong systemtime = Convert.ToUInt64(fields[2]);
+                    ulong idletime = Convert.ToUInt64(fields[3]);
+                    ulong ioWait = Convert.ToUInt64(fields[4]);
+                    ulong irq = Convert.ToUInt64(fields[5]);
+                    ulong softIrq = Convert.ToUInt64(fields[6]);
+                    ulong steal = Convert.ToUInt64(fields[7]);
+                    ulong guest = Convert.ToUInt64(fields[8]);
+                    ulong guestnice = Convert.ToUInt64(fields[9]);
+
+                    usertime = usertime - guest;
+                    nicetime = nicetime - guestnice;
+
+                    ulong idlealltime = idletime + ioWait;
+                    ulong systemalltime = systemtime + irq + softIrq;
+                    ulong virtalltime = guest + guestnice;
+
+                    CPU_WorkJiffies[i] = usertime + nicetime + systemalltime + virtalltime;
+                    CPU_TotalJiffies[i] = CPU_WorkJiffies[i] + idlealltime; // + steal
                 }
             }
             if (!this.FirstUpdate)
-            {
-                this.CPU_Usage = Helper.ShiftArray<float>(this.CPU_Usage);
-                this.CPU_Usage[0] = (float)(CPU_WorkJiffies - this.CPU_WorkJiffies) / (CPU_TotalJiffies - this.CPU_TotalJiffies);
-            }
+                for (byte i = 0; i < this.CpuCount; i++)
+                {
+                    this.CPU_Usage[i] = Helper.ShiftArray<float>(this.CPU_Usage[i]);
+                    this.CPU_Usage[i][0] = (float)(CPU_WorkJiffies[i] - this.CPU_WorkJiffies[i]) / (CPU_TotalJiffies[i] - this.CPU_TotalJiffies[i]);
+                }
             this.CPU_TotalJiffies = CPU_TotalJiffies;
             this.CPU_WorkJiffies = CPU_WorkJiffies;
 
